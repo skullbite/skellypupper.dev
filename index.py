c822fc45727a8723c4414f8bf459623719cd6cce
skullbite <mawler12@gmail.com>
2020-10-08 17:36:53 -0400

    init

---
+++ index.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, send_file, Request
+import logging, json, requests
+
+app = Flask(__name__)
+#log = logging.getLogger('werkzeug')
+#log.disabled = True
+
+try:
+    config = json.load(open("config.json"))
+except:
+    print("augu make a config.json")
+
+def get_avy(id):
+    bot_token = config.get("token")
+    if not bot_token:
+        raise Exception("Config file missing 'bot_token'.")
+    req = requests.get("https://discord.com/api/users/{id}", headers={"Authorization": f"Bot {bot_token}"})
+    if not req.status_code != 200:
+        raise Exception(req.status_code)
+    return f"https://images.discordapp.net/avatars/{id}/{req.json()['headers']}.png?size=512"
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("warning.html", error="404", message="Seems like you got somewhere you shouldn't be... maybe you should <span><a href='/'>Go Home</a></span>.")
+
+
+
+#@app.after_request
+#def after_request_func(r: Request):
+#    print(f"{r.method}: {r.environ['HTTP_X_FORWARDED_FOR']} - {r.full_path}")
+#    return r
+
+@app.route("/")
+def index():
+    return render_template("index.html", avy=get_avy(158750488563679232))
+
+@app.route("/assets/<f>")
+def assets(f):
+    return send_file(f"static/assets/{f}.png")
+
+@app.route("/stall")
+def stall():
+    return render_template("warning.html", error="Stalled!", message="oops... Looks like you hit a part of the site that isn't ready yet.<br>Check back later!")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == '__main__':
+    app.run(config.get("host") if config.get("host") else "127.0.0.1", config.get("port") if config.get("port") else 5000, debug=True)
---
+++ static/scripts/stuff.js
@@ -0,0 +1,26 @@
+const drop = () => {
+    const things = document.getElementById("drop")
+    things.style.display = "block"
+}
+
+const cutie = () => {
+    let cutieButton = document.getElementById("cutiebutton")
+    cutieButton.innerHTML = "You're a huge cutie. Check your console for more info."
+    cutieButton.disabled = true
+    cutieButton.style.textDecoration = "none"
+    console.warn("So it appears you've clicked the button you've been told not to click. You don't like to listen so you've been deemed a cutie, although i think you're quite adorable to be honest. If you didn't want this should've listened you big cutie.")
+    /* let s = document.createElement('style')
+    let css = `button:hover {
+        transition: none;
+        transform: none;
+        text-decoration: none;
+        color: rgb(204, 204, 204);
+    }`
+    if (s.styleSheet) {
+        s.styleSheet.cssText = css;
+    } else {
+        s.appendChild(document.createTextNode(css))
+    }
+    document.getElementsByTagName('head')[0].appendChild(style) */
+    
+}
